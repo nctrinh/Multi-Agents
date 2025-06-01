0036c530bdfa43fd9a933db5dca6c27cf4ab804a
Nguyễn Công Trình <nctrinh2705@gmail.com>
2025-06-01 15:02:21 +0700

    Change model provider from Google Genai to Groq

--- multi_agent/config.py
+++ multi_agent/config.py
@@ -1,41 +1,40 @@
 from dotenv import load_dotenv
 from typing import Literal, Any
+import os
+
 from langchain_tavily import TavilySearch
 from langchain.chat_models import init_chat_model
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq
 
 load_dotenv()
+
+supervisor_llm = os.getenv("SUPERVISOR_LLM")
+model_provider = os.getenv("MODEL_PROVIDER")
+agent_llm = os.getenv("AGENT_LLM")
 
 def get_web_search(max_results=3):
     return TavilySearch(max_results=max_results)
 
-def get_supervisor_llm(
-    model : Literal["gemini-2.0-flash", "gemini-1.5-flash"] = "gemini-2.0-flash"
-) -> Any:
-    return init_chat_model(model, model_provider="google_genai")
+def get_supervisor_llm() -> Any:
+    return init_chat_model(model=supervisor_llm, model_provider=model_provider)
 
 def get_web_search_llm(
-    model="gemini-2.0-flash",
     temperature=0.7,
-    top_p=0.8,
-    top_k=40,
+    top_p=0.8
 ):
-    return ChatGoogleGenerativeAI(
-        model=model,
+    return ChatGroq(
+        model=agent_llm,
         temperature=temperature,
-        top_p=top_p,
-        top_k=top_k,
+        model_kwargs={"top_p": top_p}
     )
 
 def get_math_llm(
-    model="gemini-2.0-flash",
     temperature=0.7,
-    top_p=0.8,
-    top_k=40,
+    top_p=0.8
 ):
-    return ChatGoogleGenerativeAI(
-        model=model,
+    return ChatGroq(
+        model=agent_llm,
         temperature=temperature,
-        top_p=top_p,
-        top_k=top_k,
+        model_kwargs={"top_p": top_p}
     )
