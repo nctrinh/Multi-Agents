7f7237fa3b8facc1d680d19ab163155de041e37d
Nguyễn Công Trình <nctrinh2705@gmail.com>
2025-06-04 22:20:45 +0700

    Update workflow (add cyper_kg_agent and build_knowledge_graph_agent)

--- multi_agent/graph.py
+++ multi_agent/graph.py
@@ -1,18 +1,28 @@
 from langgraph.graph import StateGraph, START, END, MessagesState
 
-from multi_agent.utils.agents import research_agent, math_agent, supervisor_agent_with_description
+from multi_agent.agents import (
+    research_agent, 
+    math_agent, 
+    cyper_kg_agent, 
+    build_knowledge_graph_agent, 
+    supervisor_agent_with_description
+)
 
 
 # CREATE MULTI-AGENT GRAPH
 workflow = (
     StateGraph(MessagesState)
     .add_node(
-        supervisor_agent_with_description, destinations=("research_agent", "math_agent")
+        supervisor_agent_with_description, destinations=("cyper_kg_agent", "build_knowledge_graph_agent", END)
     )
-    .add_node(research_agent)
-    .add_node(math_agent)
+    # .add_node(research_agent)
+    # .add_node(math_agent)
+    .add_node(cyper_kg_agent)
+    .add_node(build_knowledge_graph_agent)
     .add_edge(START, "supervisor")
-    .add_edge("research_agent", "supervisor")
-    .add_edge("math_agent", "supervisor")
+    # .add_edge("research_agent", "supervisor")
+    # .add_edge("math_agent", "supervisor")
+    .add_edge("cyper_kg_agent", "supervisor")
+    .add_edge("build_knowledge_graph_agent", "supervisor")
     .compile()
 )
