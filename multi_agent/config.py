b9d170905b2593eecc05e0ee95a9191071b08eee
Nguyễn Công Trình <nctrinh2705@gmail.com>
2025-06-04 22:14:49 +0700

    Add cypher_llm, action_build_llm, and update parameter data types.

--- multi_agent/config.py
+++ multi_agent/config.py
@@ -13,28 +13,55 @@ supervisor_llm = os.getenv("SUPERVISOR_LLM")
 model_provider = os.getenv("MODEL_PROVIDER")
 agent_llm = os.getenv("AGENT_LLM")
 
+
+# Tavily Search
 def get_web_search(max_results=3):
     return TavilySearch(max_results=max_results)
 
+# Supervisor LLM
 def get_supervisor_llm() -> Any:
     return init_chat_model(model=supervisor_llm, model_provider=model_provider)
 
+# Research LLM
 def get_web_search_llm(
-    temperature=0.7,
-    top_p=0.8
+    temperature: float = 0.7,
+    top_p: float = 0.8
 ):
     return ChatGroq(
         model=agent_llm,
-        temperature=temperature,
-        model_kwargs={"top_p": top_p}
+        temperature=float(temperature),
+        model_kwargs={"top_p": float(top_p)}
     )
 
+# Math LLM
 def get_math_llm(
-    temperature=0.7,
-    top_p=0.8
+    temperature: float = 0.7,
+    top_p: float = 0.8
 ):
     return ChatGroq(
         model=agent_llm,
-        temperature=temperature,
-        model_kwargs={"top_p": top_p}
+        temperature=float(temperature),
+        model_kwargs={"top_p": float(top_p)}
     )
+
+# Cypher LLM
+def get_cypher_llm(
+    temperature: float = 0.7,
+    top_p: float = 0.8
+):
+    return ChatGroq(
+        model=agent_llm,
+        temperature=float(temperature),
+        model_kwargs={"top_p": float(top_p)}
+    )
+
+def get_action_build_llm(
+    temperature: float = 0.7,
+    top_p: float = 0.8
+):
+    return ChatGroq(
+        model=agent_llm,
+        temperature=float(temperature),
+        model_kwargs={"top_p": float(top_p)}
+    )
+
