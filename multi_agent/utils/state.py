75636b8326463c33874d41418e19f7179631c22c
Nguyễn Công Trình <nctrinh2705@gmail.com>
2025-06-07 08:30:10 +0700

    chore: remove unused components

--- multi_agent/utils/state.py
+++ multi_agent/utils/state.py
@@ -1,8 +1,6 @@
-from typing import Dict, List, TypedDict, Annotated
+from typing import List, TypedDict, Annotated
 from langchain_core.messages import BaseMessage
 import operator
 
 class MessagesState(TypedDict):
     messages: Annotated[List[BaseMessage], operator.add]
-    # Bổ sung trường lưu kết quả tích lũy
-    accumulated_results: Annotated[Dict[str, str], operator.add]    
